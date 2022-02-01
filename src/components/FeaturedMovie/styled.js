8aedcca002bfad4b51b5fdc61f145a093e815152
Vinicius Rocha <vinirocha05@gmail.com>
2022-01-31 23:33:22 -0300

    montando p[agina de destaque

---
+++ src/components/FeaturedMovie/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Featured = styled.div`
+  min-height: 100vh;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const Transparencia = styled.div`
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to top, black);
+  z-index 
+`;
